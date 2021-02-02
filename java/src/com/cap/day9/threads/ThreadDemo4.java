3f4c1a2f20b88ba3f838a0f3e56153d040d0d9b5
Puneet Vashisht <puneetvashisht@outlook.com>
2021-02-02 16:13:03 +0530

    test

--- java/src/com/cap/day9/threads/ThreadDemo4.java
+++ java/src/com/cap/day9/threads/ThreadDemo4.java
@@ -1,15 +1,19 @@
 package com.cap.day9.threads;
 
-class RoomBookTask implements Runnable{
+class RoomBookTask implements Runnable {
 
-	int roomsBooked = 0;
-	
-	
-	public synchronized void bookRooms() {
-		for(int i=0; i<1000; i++) {
-			roomsBooked = roomsBooked + 1;
+	Integer roomsBooked = 0;
+	String str = "test";
+
+	public void bookRooms() {
+		synchronized (str) {
+			for (int i = 0; i < 1000; i++) {
+				roomsBooked = roomsBooked + 1;
+			}
 		}
+
 	}
+
 	@Override
 	public void run() {
 		this.bookRooms();
@@ -18,15 +22,14 @@ class RoomBookTask implements Runnable{
 }
 
 public class ThreadDemo4 {
-	
 
 	public static void main(String[] args) throws InterruptedException {
 		RoomBookTask runnable = new RoomBookTask();
-		
-		for(int i=0;i<100000;i++) {
-			
+
+		for (int i = 0; i < 10000; i++) {
+
 			Thread t = new Thread(runnable);
-			t.start();	
+			t.start();
 		}
 		System.out.println(Thread.currentThread().getName());
 		Thread.sleep(2000);

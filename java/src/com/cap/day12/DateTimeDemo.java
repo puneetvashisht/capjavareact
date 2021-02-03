0a31f83622136048b94801c6b774ccd4b3d40a4b
Puneet <puneetvashisht@gmail.com>
2021-02-03 15:16:18 +0530

    streams2

---
+++ java/src/com/cap/day12/DateTimeDemo.java
@@ -0,0 +1,106 @@
+package com.cap.day12;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+
+public class DateTimeDemo {
+	public static void main(String[] args) {
+		LocalDateTime timePoint = LocalDateTime.now(); // The current date and time
+
+		System.out.println(timePoint);
+
+		LocalDate localDate = LocalDate.of(2012, Month.DECEMBER, 12); // from values
+
+		System.out.println(localDate);
+
+		LocalTime localTime = LocalTime.of(17, 18); // the train I took home today
+		System.out.println(localTime);
+
+		LocalTime localTime1 = LocalTime.parse("10:15:30"); // From a String
+		System.out.println(localTime1);
+
+		LocalDate theDate = timePoint.toLocalDate();
+		Month month = timePoint.getMonth();
+		int day = timePoint.getDayOfMonth();
+		timePoint.getSecond();
+
+		System.out.println(theDate + " Month: " + month + " Day : " + day + " seconds: " + timePoint.getSecond());
+
+		// to print in a particular format
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+		String formatedDateTime = timePoint.format(format);
+
+		System.out.println("in foramatted manner " + formatedDateTime);
+
+		// Set the value, returning a new object
+		LocalDateTime thePast = timePoint.withDayOfMonth(10).withYear(2010);
+
+		System.out.println(thePast);
+
+		/*
+		 * You can use direct manipulation methods, or pass a value and field pair
+		 */
+		LocalDateTime yetAnother = thePast.plusWeeks(3).plus(3, ChronoUnit.WEEKS);
+		System.out.println(yetAnother);
+
+		// adding 1 week to the current date
+		LocalDateTime nextWeek = timePoint.minus(1, ChronoUnit.WEEKS);
+		System.out.println("Previous Week: " + nextWeek);
+
+		// Temporals
+
+		// to get the first day of next month
+		LocalDateTime dayOfNextMonth = timePoint.with(TemporalAdjusters.firstDayOfNextMonth());
+		System.out.println("Temporal " + dayOfNextMonth);
+
+//		/ You can specify the zone id when creating a zoned date time
+		ZoneId tokyo = ZoneId.of("Asia/Tokyo");
+		ZonedDateTime zoned = ZonedDateTime.of(timePoint, tokyo);
+		System.out.println(zoned.getHour());
+
+		// to get the current zone
+		ZonedDateTime currentZone = ZonedDateTime.now();
+		System.out.println("the current zone is " + currentZone.getZone());
+		ZonedDateTime tokyoZone = currentZone.withZoneSameInstant(tokyo);
+		System.out.println("tokyo time zone is " + tokyoZone);
+
+		// PEriod: It deals with date based amount of time.
+//	    Duration : It deals with time based amount of time.
+		LocalDate date1 = LocalDate.now();
+
+		LocalDate date2 = LocalDate.of(2014, Month.DECEMBER, 12);
+
+		Period gap = Period.between(date2, date1);
+		System.out.println("gap between dates " + "is a period of " + gap);
+
+		LocalTime time1 = LocalTime.now();
+		System.out.println("the current time is " + time1);
+
+		Duration fiveHours = Duration.ofHours(5);
+
+		// adding five hours to the current
+		// time and storing it in time2
+		LocalTime time2 = time1.plus(fiveHours);
+
+		System.out.println("after adding five hours " + "of duration " + time2);
+
+		Duration gapd = Duration.between(time2, time1);
+		System.out.println("duraion gap between time1" + " & time2 is " + gapd);
+
+		// Instant
+
+		Instant instant = Instant.now();
+		System.out.println(instant);
+	}
+}
--- java/src/com/cap/day12/StreamsDemo.java
+++ java/src/com/cap/day12/StreamsDemo.java
@@ -1,9 +1,17 @@
 package com.cap.day12;
 
+import static java.util.stream.Collectors.toCollection;
+
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.Set;
+import java.util.Vector;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -15,12 +23,12 @@ public class StreamsDemo {
 		Employee e1 = new Employee(1, "Ravi", 34343.34);
 		Employee e2 = new Employee(2, "Priya", 44343.34);
 		Employee e3 = new Employee(12, "Arijit", 24343.34);
-
+		Employee e4 = new Employee(15, "Amit", 24343.34);
 		List<Employee> employees = new ArrayList<>();
 		employees.add(e1);
 		employees.add(e2);
 		employees.add(e3);
-		
+		employees.add(e4);
 		
 		Stream<Employee> stream = employees.stream();
 		
@@ -41,16 +49,58 @@ public class StreamsDemo {
 			System.out.println(minimumSalariedEmployee.get().getSalary());
 		}
 		
-////		1. create a stream 
-//		stream
-////		intermediate operation (zero to many)
-//		.filter(e -> e.getSalary() < 40000)
-//		.map(e -> {
-//			e.incrementSalary();
-//			return e.getSalary();
-//		})
-////		terminal operation (one)
+		
+		stream = employees.stream();
+//		1. create a stream 
+		
+		
+		List<Employee> filteredList = stream
+//		intermediate operation (zero to many)
+		.filter(e -> e.getSalary() < 40000)
+		.map(e -> {
+			e.incrementSalary();
+			return e;
+		})
+//		terminal operation (one)
 //		.forEach((e)-> System.out.println(e));
+//		.forEach(System.out::println);
+//		.collect(toList());
+		.collect(toCollection(LinkedList::new));
+		
+//		System.out.println(filteredList);
+		
+		
+		
+
+		// Advanced collectors--- 
+		//1. Joining
+		
+		
+//				String empNames = employees.stream()
+//					      .map(Employee::getName)
+//					      .collect(Collectors.joining(", "));
+//				System.out.println(empNames);
+				
+				
+				
+				//2. summary
+//				DoubleSummaryStatistics stats = employees.stream()
+//					      .collect(Collectors.summarizingDouble(Employee::getSalary));
+//				System.out.println(stats);
+//				System.out.println(stats.getMin());
+//				
+				//3. partitioningBy
+//				Map<Boolean, List<Employee>> isEven = employees.stream().collect(
+//					      Collectors.partitioningBy(e -> e.getId() % 2 == 0));
+//				System.out.println(isEven.get(true));
+//				System.out.println(isEven.get(false));
+//				
+//				//4. groupingBy
+				Map<Character, List<Employee>> groupByAlphabet = employees.stream().collect(
+					      Collectors.groupingBy(e -> new Character(e.getName().charAt(0))));
+				System.out.println(groupByAlphabet);
+//				
+				
 
 		
 
